7ee866c5526e1a3af6951be27babec1176dc06ea
KOITT <KOITT@KOITT-PC>
2018-05-23 21:28:03 +0900

    34쪽 예제까지 완료

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World!!");
+		System.out.println("Hello, World!");
 		System.out.println("안녕, 세계!!");
 		System.out.println("조은향");
 	}
