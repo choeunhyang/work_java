52edbdace02f0b95f413ac4e417baef560aaed4b
koitt-03-P <koitt-03-P@koitt-03-P-PC>
2018-06-05 21:48:28 +0900

    예제

---
+++ ch04/src/ch04/Ex01.java
@@ -0,0 +1,18 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String tmp = scan.nextLine();
+		int x = Integer.parseInt(tmp);
+		if() {
+			System.out.println("true");
+		}else {
+		System.out.println("flase");
+		}
+	}
+
+}
