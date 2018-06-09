7e798c7f37f00fd7178cb2b95b6e5726a29eced6
koitt-03-P <koitt-03-P@koitt-03-P-PC>
2018-06-09 16:45:11 +0900

    ..

--- ch04/src/ch04/Ch04Ex02_05.java
+++ ch04/src/ch04/Ch04Ex02_05.java
@@ -1,10 +1,21 @@
 package ch04;
 
+import java.util.Scanner;
+
 public class Ch04Ex02_05 {
 
 	public static void main(String[] args) {
-		
 
+		Scanner sc = new Scanner(System.in);
+		 do {
+			 int a = sc.nextInt();
+			 if (a%3==0) {
+				 System.out.println(a/3);
+			 }else if(a ==-1) {
+				 break;
+			 }
+		 }while(true);
+		 
 	}
 
 }
--- ch04/src/ch04/Ex14.java
+++ ch04/src/ch04/Ex14.java
@@ -4,14 +4,14 @@ public class Ex14 {
 
 	public static void main(String[] args) {
 		// 1~100 사이의 임의의 값을 얻어서 answer에 저장한다.
-		int answer = (int)(Math.random() * 100);
+		int answer = (int)(Math.random() * 100)+1;
 		int input = 0; // 사용자입력을 저장할 공간
 		int count = 0; // 시도횟수를 세기위한 변수
 		// 화면으로 부터 사용자입력을 받기 위해서 Scanner 클래스 사용
 		java.util.Scanner s = new java.util.Scanner(System.in);
 		do {
 			count++;
-			System.out.print("1 과 100 사이의 값을 입력하세요 :");
+			System.out.print("1 과 100 사이의 값을 입력하세요 : ");
 			input = s.nextInt(); //  . 입력받은 값을 변수 input 에 저장한다
 			
 			// 1.정답일 경우
